6b1f1140b8beb83562f773f8aa5801fd1e9bccaf
Aleksandra Brytanczyk <olcia.brytanczyk@gmail.com>
2020-05-19 13:25:29 +0200

    zadanie3

---
+++ Zadania_Mysłajek_Wiśniewski/Zadanie2/src/Daytime.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+class DateTime {
+
+    public static void main(String[] args) {
+
+        String address = "time.nist.gov";
+        int port = 13;
+
+        /*
+        Utwórz gniazdo na porcie przypisanym dla protokołu  DAYTIME, pobierz z niego strumień wejściowy i odczytaj odpowiedź serwera.
+        Nie zapomnij o zamknięciu strumienia i gniazda.
+        */
+
+        InetSocketAddress socketAddress = new InetSocketAddress(address, port);
+        Socket socket = new Socket();
+        try {
+            socket.connect(socketAddress);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String time = in.readLine();
+            System.out.println(time);
+            socket.shutdownInput();
+            socket.shutdownOutput();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
